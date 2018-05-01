ff7b23f2c852a3b6523a45f40cd973adc9593ff6
Jing Chun Tan <jtan1993@gmail.com>
2018-05-01 16:55:17 +0100

    Create snapshotToTexture.js

---
+++ snapshotToTexture.js
@@ -0,0 +1,23 @@
+let state = 0;
+let dataTexture = Z.DataTexture();
+
+Z.screen.on("resize", (w,h)=> {
+    symbol.nodes.Snapshot.scale([w/h,1,1]);
+    
+})
+
+symbol.nodes.Hotspot.on("pointerup", (e) => {
+	if(state === 0 ) {
+	    Z.device.snapshotToTexture(dataTexture, () => {
+	        symbol.nodes.Snapshot.skin(dataTexture);
+	        symbol.controllers.States.elements.photo_taken.reset();
+	        state++;
+	    })
+	}
+	if(state === 1) {
+	    symbol.controllers.States.elements.fullscreen.reset();
+	    Z.device.snapshot("", ()=> {
+	        state = 0;
+	    });
+	}
+});
